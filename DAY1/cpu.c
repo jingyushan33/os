7ae5f4f546b2c3f7022ee6d1b1270757162a8968
ZR20040284-井雨姗 <jingyushan@zhongruigroup.com>
2022-08-31 11:51:10 +0800

    feat:init

---
+++ DAY1/cpu.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+    //argc 是argument count的缩写表示传入main函数中的参数个数，包括这个程序本身
+    //argv 是 argument vector的缩写表示传入main函数中的参数列表，其中argv[0]表示这个程序的名字
+    printf("%s\n",argv[1]);
+    if (argc != 2) {
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
+    }
+    char *str = argv[1];
+    while (1) {
+        sleep(1);
+        printf("%s\n", str);
+    }
+    return 0;
+}
