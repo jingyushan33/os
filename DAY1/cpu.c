3308a0e83c0dce9f1931e77d1cbd70009244308e
ZR20040284-井雨姗 <jingyushan@zhongruigroup.com>
2022-09-09 15:58:53 +0800

    init

--- DAY1/cpu.c
+++ DAY1/cpu.c
@@ -4,8 +4,7 @@
 #include <assert.h>
 #include <unistd.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     //argc 是argument count的缩写表示传入main函数中的参数个数，包括这个程序本身
     //argv 是 argument vector的缩写表示传入main函数中的参数列表，其中argv[0]表示这个程序的名字
